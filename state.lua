ff032f529dd1d7608ed2ec15adfe442108291de3
rajdakin <rajdakin@gmail.com>
2018-08-14 18:52:40 +0200

    Added an exit marker when all states are popped

--- state.lua
+++ state.lua
@@ -7,17 +7,23 @@ local utilmodule = require(import_prefix .. "util")
 local classmodule = require(import_prefix .. "class")
 
 local StateManager = class(function(self, main_state)
+	self.__exit = false
+	
 	self.__main_states, self.__main_count = {main_state}, 1
 	self.__states, self.__count = {main_state}, 1
 end)
 
-function StateManager:getState() return self:getStatesStack() end
+function StateManager:mustExit() return self.__exit end
 
 function StateManager:getStatesStack()     return self.__states      end
 function StateManager:getMainStatesStack() return self.__main_states end
 
 function StateManager:getStatesCount()     return self.__count      end
 function StateManager:getMainStatesCount() return self.__main_count end
+
+function StateManager:getState()
+	return self:getStatesStack()
+end
 
 function StateManager:pushMainState(main_state)
 	self.__main_states[self.__main_count + 1] = {main_state}
@@ -27,7 +33,8 @@ function StateManager:pushMainState(main_state)
 end
 
 function StateManager:popMainState()
-	if self.__main_count <= 1 then os.exit(0) end
+	if self.__main_count == 1 then self.__exit = true
+	elseif slf.__main_count == 0 then return nil end
 	
 	while self.__states[2] do self:popState() end
 	

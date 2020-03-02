f1a31e0ac144348d97da1232e17de31ab70403e6
rajdakin <rajdakin@gmail.com>
2020-03-02 22:46:23 +0100

    [CONFIG] Implemented the settings.cfg file support

--- config.lua
+++ config.lua
@@ -8,6 +8,40 @@ local errormodule = require(import_prefix .. "error")
 local utilmodule = load_module(import_prefix .. "util", true)
 
 local classmodule = load_module(import_prefix .. "class", true)
+local filemodule = load_module(import_prefix .. "file", true)
+
+local Configuration = abst_class(function(self, configDS)
+	self.__listeners = {}
+	
+	self:updateConfig(configDS)
+end)
+
+--[[ addListener - add a listener to the configuration and get notified at each configuration update
+	callback - function that takes as a parameter the updated configuration
+]]
+function Configuration:addListener(listener, callback)
+	self.__listeners[listener] = callback
+end
+function Configuration:removeListener(listener)
+	local ret = self.__listeners[listener] ~= nil
+	self.__listeners[listener] = nil
+	return ret
+end
+
+function Configuration:notifyListeners()
+	for l, f in pairs(self.__listeners) do
+		f(self)
+	end
+end
+
+function Configuration:updateConfig(ds)
+	if ds then self.__ds = ds end
+	self:__updateSelf()
+	self:notifyListeners()
+end
+
+Configuration:__addAbstract("__updateSelf")
+Configuration:__addAbstract("_updateDS")
 
 --[[ LevelConfig - the level configuration class.
 	Holds whether the minimap should display, whether the map should be displayable,
@@ -15,13 +49,24 @@ local classmodule = load_module(import_prefix .. "class", true)
 	
 	levelConfiguration - the level configuration table
 ]]
-local LevelConfig = class(function(self, levelConfiguration)
-	self.__displayMinimap = levelConfiguration["minimapDisplay"]
-	self.__displayMap = levelConfiguration["mapDisplayable"]
-	self.__minimapViewingSize = levelConfiguration["minimapViewingSize"]
-	self.__mapOffset = {0, levelConfiguration["mapYoffset"]}
+local LevelConfig = class(nil, Configuration)
+LevelConfig:__implementAbstract("__updateSelf", function(self)
+	self.__displayMinimap = self.__ds:get("displayMinimap")
+	self.__displayMap = self.__ds:get("displayMap")
 	
-	self.__difficulty = levelConfiguration["difficulty"]
+	self.__minimapViewingSize = self.__ds:get("minimapViewingSize")
+	self.__mapOffset = self.__ds:get("mapOffset")
+	
+	self.__difficulty = self.__ds:get("difficulty")
+end)
+LevelConfig:__implementAbstract("_updateDS", function(self)
+	self.__ds:set("displayMinimap", self.__displayMinimap, "boolean")
+	self.__ds:set("displayMap", self.__displayMap, "boolean")
+	
+	self.__ds:set("minimapViewingSize", self.__minimapViewingSize, "array")
+	self.__ds:set("mapOffset", self.__mapOffset, "array")
+	
+	self.__ds:set("difficulty", self.__difficulty, "number")
 end)
 
 function LevelConfig:doesDisplayMinimap() return self.__displayMinimap  end
@@ -42,18 +87,30 @@ function LevelConfig:getDifficulty() return self.__difficulty end
 	levelManagerConfiguration - the level manager configuration table
 	levelConfiguration - the default levels configuration table used on loading levels
 ]]
-local LevelManagerConfig = class(function(self, levelManagerConfiguration, levelConfiguration)
-	self.__levelConfig = LevelConfig(levelConfiguration)
+local LevelManagerConfig = class(nil, Configuration)
+LevelManagerConfig:__implementAbstract("__updateSelf", function(self)
+	if not self.__levelConfig then self.__levelConfig = LevelConfig(self.__ds:getAsDataStream("levelConfig"))
+	else self.__levelConfig:updateConfig(self.__ds:getAsDataStream("levelConfig")) end
 	
-	self.__loadTestLevels = levelManagerConfiguration["loadTestLevels"]
+	self.__loadTestLevels = self.__ds:get("loadTestLevels")
+end)
+LevelManagerConfig:__implementAbstract("_updateDS", function(self)
+	self.__levelConfig:_updateDS()
+	self.__ds:setSubDataStream("levelConfig", self.__levelConfig.__ds)
+	
+	self.__ds:set("loadTestLevels", self.__loadTestLevels, "boolean")
 end)
 
 function LevelManagerConfig:getLevelConfig() return self.__levelConfig end
 
 function LevelManagerConfig:doLoadTestLevels() return self.__loadTestLevels end
 
-local KeyboardConfig = class(function(self, keyboardConfiguration)
-	self.__directions = keyboardConfiguration["directions"]
+local KeyboardConfig = class(nil, Configuration)
+KeyboardConfig:__implementAbstract("__updateSelf", function(self)
+	self.__directions = self.__ds:get("directions")
+end)
+KeyboardConfig:__implementAbstract("_updateDS", function(self)
+	self.__ds:set("directions", self.__directions, "array")
 end)
 
 function KeyboardConfig:getDirectionsKey() return self.__directions end
@@ -77,9 +134,15 @@ end
 	
 	levelConfiguration - the level configuration table
 ]]
-local ConsoleConfig = class(function(self, consoleConfiguration)
-	self.__logLevel = min(max(floor(tonumber(consoleConfiguration["logLevel"])), 0), 4)
-	self.__developerMode = consoleConfiguration["developerMode"]
+local ConsoleConfig = class(nil, Configuration)
+ConsoleConfig:__implementAbstract("__updateSelf", function(self)
+	self.__logLevel = self.__ds:get("logLevel")
+	self.__logLevel = min(max(floor(self.__logLevel), 0), 4)
+	self.__developerMode = self.__ds:get("developerMode")
+end)
+ConsoleConfig:__implementAbstract("_updateDS", function(self)
+	self.__ds:set("logLevel", min(max(floor(self.__logLevel), 0), 4), "number")
+	self.__ds:set("developerMode", self.__developerMode, "boolean")
 end)
 
 function ConsoleConfig:getLogLevel()             return self.__logLevel             end
@@ -87,32 +150,55 @@ function ConsoleConfig:isLogLevelValid(logLevel) return self.__logLevel >= logLe
 function ConsoleConfig:isDeveloperMode()         return self.__developerMode        end
 
 --[[ Config - the global configuration class [singleton]
-	Holds the level manager configuration and the console configuration.
+	Holds the level manager, keyboard and console configurations.
 	
 	configuration - the global configuration
 ]]
-local Config = class(function(self, configuration)
-	self.__levelManagerConfig = LevelManagerConfig(configuration["levelManagerConfiguration"], configuration["levelConfiguration"])
-	self.__keyboardConfig = KeyboardConfig(configuration["keyboardConfiguration"])
-	self.__consoleConfig = ConsoleConfig(configuration["consoleConfiguration"])
+local Config = class(function(self, filename)
+	self.__ds = DataStream()
+	
+	self.__filename = filename
+	
+	self.configs = {levelManager = LevelManagerConfig, keyboard = KeyboardConfig, console = ConsoleConfig}
+	self:readConfig()
 end)
 
 function Config:getLevelManagerConfig() return self.__levelManagerConfig end
 function Config:getKeyboardConfig() return self.__keyboardConfig end
 function Config:getConsoleConfig() return self.__consoleConfig end
 
+function Config:readConfig()
+	self.__ds:read(self.__filename)
+	
+	self:updateConfig()
+end
+
+function Config:writeConfig()
+	if not self.__ds then
+		self.__ds = DataStream()
+		
+		self.__ds:setSubDataStream("levelManager", self.__levelManagerConfig.__ds)
+		self.__ds:setSubDataStream("keyboard", self.__keyboardConfig.__ds)
+		self.__ds:setSubDataStream("console", self.__consoleConfig.__ds)
+	end
+	
+	self.__ds:write(self.__filename)
+end
+
+function Config:updateConfig()
+	for cfg, cls in pairs(self.configs) do
+		local cfgName = "__" .. cfg .. "Config"
+		if not self[cfgName] then self[cfgName] = cls(self.__ds:getAsDataStream(cfg))
+		else self[cfgName]:updateConfig(self.__ds:getAsDataStream(cfg)) end
+	end
+end
+function Config:updateDataStream()
+	for cfg, cls in pairs(self.configs) do
+		local cfgName = "__" .. cfg .. "Config"
+		self[cfgName]:_updateDS()
+		self.__ds:setSubDataStream(cfg, self[cfgName].__ds)
+	end
+end
+
 -- currentConfig - the configuration singleton
-currentConfig = Config({
-    ["levelManagerConfiguration"] = {["loadTestLevels"] = false},
-    ["levelConfiguration"] = {["minimapDisplay"] = true,
-                              ["minimapViewingSize"] = {3, 3},
-                              ["mapDisplayable"] = true,
-                              ["mapYoffset"] = 7,
-                              ["difficulty"] = 3},
-    ["keyboardConfiguration"] = {["directions"] = {["up"] = "u",
-                                                   ["down"] = "d",
-                                                   ["left"] = "l",
-                                                   ["right"] = "r"}},
-    ["consoleConfiguration"] = {["logLevel"] = 2,
-                                ["developerMode"] = false}
-})
+currentConfig = Config("settings.cfg")
